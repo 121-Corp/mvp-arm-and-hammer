fa92ac11933d552e0ea66743e7b7930f42f3b6d2
Ricardo Tapia <rtapia@121corp.com>
2024-08-29 16:28:59 -0600

    Se agregan vistas y se ajusta menu princiapl

--- resources/views/components/menu.blade.php
+++ resources/views/components/menu.blade.php
@@ -5,36 +5,41 @@
 </style>
 <div class="hidden bg-[#0000004a] h-screen w-full fixed z-[1] top-0 left-0" id="backgraound">
 </div>
-<div class="flex w-11/12 lg:w-full fixed right-0 top-0 left-0 bottom-0 mx-auto items-center xl:px-16 h-[50px] lg:h-[80px] z-[10] rounded-[37px] lg:rounded-[0px] bg-white" id="mainMenu">
-   <nav
-       class="navbar flex absolute justify-between items-center w-full h-full top-0 right-0 bg-white rounded-[37px] lg:rounded-[0px] mt-[6px] lg:mt-0 lg:h-[70px] text-[#1E1E1E]">
-       <div class="m-2 md:m-8 lg:p-0  w-3/12 flex justify-end">
-           <img class="ml-5 lg:mx-0 lg: cursor-pointer" src="{{ asset('images/general/logo_x1.png') }}"
+<div class="flex w-11/12 lg:w-full fixed right-0 top-0 left-0 bottom-0 mx-auto items-center xl:px-16 h-[74px] lg:h-[80px] z-[10] rounded-[37px] lg:rounded-[0px]" id="mainMenu">
+   <nav class="navbar flex absolute justify-between items-center w-full top-0 right-0 rounded-[37px] lg:rounded-[0px] lg:mt-0 lg:h-full text-[#1E1E1E]">
+       <div class="m-2 md:m-8 lg:p-0 w-2/12 flex justify-end h-full relative">
+           <img class="ml-5 h-auto cursor-pointer lg:w-[124px] lg:absolute lg:top-5 lg:right-0" src="{{ asset('images/general/logo_x2.png') }}"
                onclick="goHome();"  alt="logo"/>
        </div>
-       <div class="mr-[20px] lg:mr-[165px] lg:w-9/12 h-full">
-           <div class="flex justify-between lg:items-center">
-               <div class="hidden lg:flex text-[#1E1E1E] lg:items-center font-semibold w-full">
-                   <div class="hidden sm:-my-px sm:ml-6 sm:flex items-center sm:space-x-8 w-full justify-between">
-                       <a class="p-0 no-underline text-[20px] items-center leading-5 font-sans font-semibold" href="{{route('home')}}">Home</a>
-                       <a class="p-0 no-underline text-[20px] items-center leading-5 font-sans font-semibold" href="" target="_blank">Dairy</a>
-                       <a class="p-0 no-underline text-[20px] items-center leading-5 font-sans font-semibold" href="" target="_blank">Contact</a>
-                       <div class="m-5 lg:m-4">
-                           <a href="#contact"
-                               class="h-[50px] inline-flex justify-center font-sans
-                                          no-underline items-center m-0 py-[10px] px-[37px] text-[16px] leading-[30px] font-semibold text-center
-                                          text-white bg-[#004C8D] border hover:bg-white hover:border-[#006E8F] hover:text-[#006E8F]">
-                                          Find a Distributor
-                           </a>
-                       </div>
+       <div class="mr-[20px] lg:mr-[165px] lg:w-10/12 h-full">
+           {{-- <div class="flex justify-between lg:items-center"> --}}
+               {{-- <div class="hidden lg:flex text-[#1E1E1E] lg:items-center font-semibold w-full"> --}}
+                   <div class="hidden h-full text-[#1E1E1E] sm:-my-px sm:ml-6 sm:flex items-center sm:space-x-8 w-full justify-between">
+                        <div class="w-3/12 text-center">
+                            <a class="p-0 no-underline text-base lg:text-[14px] items-center leading-[1px] font-semibold uppercase" href="{{route('home')}}">Home</a>
+                        </div>
+                        <div class="w-3/12 text-center">
+                            <a class="p-0 no-underline text-base lg:text-[14px] items-center leading-[1px] font-semibold uppercase" href="" target="_blank">Dairy</a>
+                        </div>
+                        <div class="w-3/12 text-center">
+                            <a class="p-0 no-underline text-base lg:text-[14px] items-center leading-[1px] font-semibold uppercase" href="" target="_blank">Contact</a>
+                        </div>
+                        <div class="w-3/12 text-center">
+                            <a href=""
+                                class="h-[50px] inline-flex justify-center font-sans
+                                            no-underline items-center m-0 py-[10px] px-[37px] text-[16px] leading-[30px] font-semibold text-center
+                                            text-white bg-[#004C8D] border hover:bg-white hover:border-[#006E8F] hover:text-[#006E8F]">
+                                            Find a Distributor
+                            </a>
+                        </div>
                    </div>
-               </div>
+               {{-- </div> --}}
                <div class="-mr-2 flex items-center lg:hidden">
                    <button id="mobileMenuButton" class="inline-flex items-center justify-center p-2">
-                       <img src="{{ asset('favicon.ico') }}" alt="menu">
+                       <img src="{{ asset('images/general/menu_x2.png') }}" class="h-auto w-[30px]" alt="menu">
                    </button>
                </div>
-           </div>
+           {{-- </div> --}}
        </div>
        <!-- Mobile Menu -->
        <div id="mobileMenu"
---
+++ resources/views/representative.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+@section('title')
+<title>{{ config('app.name') ?? "A&H" }}</title>
+@endsection
+
+@section('sidebar')
+    @parent
+@endsection
+@section('content')
+
+@endsection
